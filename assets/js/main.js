9afc3f6d6d2519b4ee2ec30fd33520bc2f3c79cd
WarrenGM <warreng.martin@gmail.com>
2025-02-19 16:51:38 -0700

    Added more pages and HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const recentPost = document.querySelector(".recent-post");
   if (recentPost) {
     recentPost.classList.add("shimmer-effect");
+    console.log("Shimmer effect applied"); // Debug line
   }
 });
